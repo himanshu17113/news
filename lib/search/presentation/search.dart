a462aec732bfbdd90c61198456df395fbe857f83
Himanshu <himanshu17113@gmail.com>
2024-01-07 13:10:38 +0530

    changed the text feild in news

--- lib/search/presentation/search.dart
+++ lib/search/presentation/search.dart
@@ -11,18 +11,15 @@ class SeachScreen extends StatelessWidget {
       appBar: PreferredSize(
         preferredSize: Size.fromHeight(context.deviceHeight() * .2),
         child: ColoredBox(
-          color: colorScheme.secondaryContainer,
-          child: Padding(
-            padding: const EdgeInsets.only(bottom: 5, top: 40),
+          color: colorScheme.surfaceVariant,
+          child: const Padding(
+            padding: EdgeInsets.only(bottom: 5, top: 40),
             child: TextField(
                 cursorWidth: 1.5,
                 keyboardType: TextInputType.text,
-                onSubmitted: (value) {},
+                // onSubmitted: (value) {},
                 // controller: ,
-                style: const TextStyle(
-                    fontSize: 12,
-                    fontWeight: FontWeight.w300,
-                    color: Color(0xFF666666)),
+
                 decoration: InputDecoration(
                   hintText: "Search for topics,location",
                   // hintStyle: GoogleFonts.poppins(
@@ -30,39 +27,21 @@ class SeachScreen extends StatelessWidget {
                   //   letterSpacing: 0.3,
                   //   color: Colors.grey,
                   // ),
-                  filled: true,
-                  fillColor: colorScheme.surfaceVariant,
-                  prefixIcon: const Icon(Icons.arrow_back_ios_new_rounded),
+
+                  prefixIcon: Icon(Icons.arrow_back_ios_new_rounded),
                   //   suffixIcon: Icon(Icons.)
                   contentPadding:
-                      const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                  border: OutlineInputBorder(
-                      borderSide: BorderSide(
-                        color: Colors.grey.withOpacity(0.5),
-                        width: 2.0,
-                      ),
-                      borderRadius: BorderRadius.circular(15)),
-                  errorBorder: const OutlineInputBorder(
-                      borderRadius: BorderRadius.zero,
-                      borderSide: BorderSide.none),
-                  enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                        color: Colors.grey.withOpacity(0.5),
-                        width: 2.0,
-                      ),
-                      borderRadius: BorderRadius.circular(15)),
-                  disabledBorder: const OutlineInputBorder(
-                      borderRadius: BorderRadius.zero,
-                      borderSide: BorderSide.none),
-                  focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                        color: Colors.grey.withOpacity(0.01),
-                        width: 5,
-                      ),
-                      borderRadius: BorderRadius.circular(15)),
-                  focusedErrorBorder: const OutlineInputBorder(
-                      borderRadius: BorderRadius.zero,
-                      borderSide: BorderSide.none),
+                      EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                  border: InputBorder.none,
+
+                  // errorBorder: const OutlineInputBorder(
+                  //     borderRadius: BorderRadius.zero,
+                  //     borderSide: BorderSide.none),
+                  enabledBorder: InputBorder.none,
+                  //  disabledBorder: const OutlineInputBorder(
+                  //     borderRadius: BorderRadius.zero,
+                  //     borderSide: BorderSide.none),
+                  focusedBorder: InputBorder.none,
                 )),
           ),
         ),
